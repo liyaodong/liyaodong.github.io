dcb2b1f25457d0e142bffe511d28ef010fed5b7f
Yaodong Li <mail@liyaodong.com>
2023-12-17 14:41:04 +0800

    Update en version resume and remove zh version

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 const gulp = require('gulp');
 const i18n = require('gulp-html-i18n');
 
-gulp.task('default', function() {
-  const dest  = './';
-  const index = './src/resume.html';
+gulp.task('default', function () {
+    const dest = './';
+    const index = './src/resume.html';
 
-  return gulp.src(index)
-    .pipe(i18n({
-      langDir: './lang',
-      trace: true,
-      defaultLang: 'en-US',
-    }))
-    .pipe(gulp.dest(dest));
+    return gulp
+        .src(index)
+        .pipe(
+            i18n({
+                langDir: './lang',
+                defaultLang: 'en-US',
+                renderEngine: 'mustache',
+            })
+        )
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['./src/**/*.html', './lang/**/*.yaml'], gulp.series('default'));
 });
